be5578e603738194e719217584e60fcb0d4e5c9c
Sam Aquino <sheila.sam.aquino@gmail.com>
2025-03-13 16:14:41 -0600

    ina

--- config/logging.php
+++ config/logging.php
@@ -60,7 +60,7 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => env('LOG_PATH', '/tmp/laravel.log'),
+            'path' => env('LOG_PATH', '/tmp/laravel_logs'),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
